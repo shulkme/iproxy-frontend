fbcd51218122ffa7fb1c86b81e96f81f5d5f7d24
shulksteve <shulk.work@gmail.com>
2025-08-28 21:26:38 +0800

    update

--- src/app/[locale]/(app)/partners/page.tsx
+++ src/app/[locale]/(app)/partners/page.tsx
@@ -1,3 +1,20 @@
+import ListCard from '@/app/[locale]/(app)/partners/components/list-card';
+import { AntdTitle } from '@/components/antd';
+
 export default function Page() {
-  return <></>;
+  return (
+    <div className="max-w-[1600px] mx-auto p-8">
+      <div>
+        <AntdTitle level={4}>Anti-Detection Browsers</AntdTitle>
+        <div className="grid gap-6 grid-cols-[repeat(auto-fill,minmax(300px,_1fr))]">
+          <ListCard />
+          <ListCard />
+          <ListCard />
+          <ListCard />
+          <ListCard />
+          <ListCard />
+        </div>
+      </div>
+    </div>
+  );
 }
