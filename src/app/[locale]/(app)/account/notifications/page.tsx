1e3e8c7425037d2d417a12eaa6504d7e6c08d0fd
shulksteve <shulk.work@gmail.com>
2025-09-04 11:41:47 +0800

    update

--- src/app/[locale]/(app)/account/notifications/page.tsx
+++ src/app/[locale]/(app)/account/notifications/page.tsx
@@ -1,26 +1,20 @@
+'use client';
 import {
   AntdList,
   AntdListItem,
   AntdListMeta,
   AntdParagraph,
 } from '@/components/antd';
-import {
-  RiEditLine,
-  RiHomeOfficeLine,
-  RiPinDistanceLine,
-  RiServerLine,
-  RiSmartphoneLine,
-  RiWalletLine,
-} from '@remixicon/react';
-import { Avatar, Button, Card, ConfigProvider, Switch } from 'antd';
+import { RiPercentLine, RiVipCrown2Line, RiWalletLine } from '@remixicon/react';
+import { Avatar, Card, ConfigProvider, Switch } from 'antd';
+import { useTranslations } from 'next-intl';
 
 export default function Page() {
+  const t = useTranslations('app.pages.account.notifications');
   return (
     <>
       <Card>
-        <AntdParagraph strong>
-          You&#39;ll get an email when your balance drops below the set amount.
-        </AntdParagraph>
+        <AntdParagraph strong>{t('tips')}</AntdParagraph>
         <ConfigProvider
           theme={{
             components: {
@@ -32,15 +26,7 @@ export default function Page() {
         >
           <AntdList size="large">
             <AntdListItem
-              actions={[
-                <Button
-                  type="link"
-                  size="small"
-                  key="edit"
-                  icon={<RiEditLine size={16} />}
-                />,
-                <Switch key="switch" />,
-              ]}
+              actions={[<Switch key="switch" defaultValue={true} />]}
             >
               <AntdListMeta
                 className="[&_h4]:font-medium"
@@ -49,104 +35,36 @@ export default function Page() {
                     <RiWalletLine size={18} />
                   </Avatar>
                 }
-                title={'Wallet'}
-                description={'Balance Threshold: $ 500'}
+                title={t('items.wallet.title')}
+                description={t('items.wallet.desc')}
               />
             </AntdListItem>
             <AntdListItem
-              actions={[
-                <Button
-                  type="link"
-                  size="small"
-                  key="edit"
-                  icon={<RiEditLine size={16} />}
-                />,
-                <Switch key="switch" />,
-              ]}
+              actions={[<Switch key="switch" defaultValue={true} />]}
             >
               <AntdListMeta
                 className="[&_h4]:font-medium"
                 avatar={
                   <Avatar className="bg-(--ant-color-primary-bg) text-(--ant-color-primary)">
-                    <RiPinDistanceLine size={18} />
+                    <RiVipCrown2Line size={18} />
                   </Avatar>
                 }
-                title={'Residential Proxies'}
-                description={
-                  'Expiration Threshold: 1 Days, Traffic Threshold: 1000 MB'
-                }
+                title={t('items.subscription.title')}
+                description={t('items.subscription.desc')}
               />
             </AntdListItem>
             <AntdListItem
-              actions={[
-                <Button
-                  type="link"
-                  size="small"
-                  key="edit"
-                  icon={<RiEditLine size={16} />}
-                />,
-                <Switch key="switch" />,
-              ]}
+              actions={[<Switch key="switch" defaultValue={true} />]}
             >
               <AntdListMeta
                 className="[&_h4]:font-medium"
                 avatar={
                   <Avatar className="bg-(--ant-color-primary-bg) text-(--ant-color-primary)">
-                    <RiHomeOfficeLine size={18} />
+                    <RiPercentLine size={18} />
                   </Avatar>
                 }
-                title={'Static ISP Proxies'}
-                description={
-                  'Expiration Threshold: 1 Days, Traffic Threshold: 1000 MB'
-                }
-              />
-            </AntdListItem>
-            <AntdListItem
-              actions={[
-                <Button
-                  type="link"
-                  size="small"
-                  key="edit"
-                  icon={<RiEditLine size={16} />}
-                />,
-                <Switch key="switch" />,
-              ]}
-            >
-              <AntdListMeta
-                className="[&_h4]:font-medium"
-                avatar={
-                  <Avatar className="bg-(--ant-color-primary-bg) text-(--ant-color-primary)">
-                    <RiServerLine size={18} />
-                  </Avatar>
-                }
-                title={'Datacenter Proxies'}
-                description={
-                  'Expiration Threshold: 1 Days, Traffic Threshold: 1000 MB'
-                }
-              />
-            </AntdListItem>
-            <AntdListItem
-              actions={[
-                <Button
-                  type="link"
-                  size="small"
-                  key="edit"
-                  icon={<RiEditLine size={16} />}
-                />,
-                <Switch key="switch" />,
-              ]}
-            >
-              <AntdListMeta
-                className="[&_h4]:font-medium"
-                avatar={
-                  <Avatar className="bg-(--ant-color-primary-bg) text-(--ant-color-primary)">
-                    <RiSmartphoneLine size={18} />
-                  </Avatar>
-                }
-                title={'Mobile Proxies'}
-                description={
-                  'Expiration Threshold: 1 Days, Traffic Threshold: 1000 MB'
-                }
+                title={t('items.discounts.title')}
+                description={t('items.discounts.desc')}
               />
             </AntdListItem>
           </AntdList>
